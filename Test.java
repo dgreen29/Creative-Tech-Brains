3e3678f75d7d1cf09b884724ec60ec5adda0cbd6
Vin <72899108+Kaz-png@users.noreply.github.com>
2023-10-10 05:13:52 +0000

    testing my ability to make commits.
    testing my ability to make commits.

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
+    //just seeing if i can make commits properly.
     public static void main(String[] theArgs) {
-        System.out.println("Testing Github");
+        System.out.println("Vin is Testing Github");
     }
 }
