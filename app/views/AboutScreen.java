07aeea48a1fb6c37942db6e41976e8b47ce13ca6
zarifzm <zarif.mazumder@seattle.gov>
2023-11-06 20:23:39 -0800

    Project structured

---
+++ app/views/AboutScreen.java
@@ -0,0 +1,4 @@
+
+public final class AboutScreen {
+    public AboutScreen() {}
+}
---
+++ app/views/ApplicationView.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import javax.swing.*;
+
+public class ApplicationView {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+    }
+}
