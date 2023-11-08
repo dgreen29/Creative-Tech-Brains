9fbd204871337fd87baab45fdbd78ad8c4cd4854
zarifzm <zarif.mazumder@seattle.gov>
2023-11-07 22:10:58 -0800

    test commit

--- app/Main.java
+++ app/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new ApplicationView();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        frame.setVisible(true); //
     }
 }
