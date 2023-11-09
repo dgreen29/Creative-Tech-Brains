7a3f525255a76b3d014a3566ad2a2b291384b098
Vin <72899108+Kaz-png@users.noreply.github.com>
2023-11-09 05:42:02 -0800

    Update ApplicationView.java

--- app/views/ApplicationView.java
+++ app/views/ApplicationView.java
@@ -7,13 +7,44 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/*
+ * @author Darrell Green, Jr. (DJ Green)
+ * @author Zarif Mazumder
+ * @author Harman Singh
+ * @author Vindhriko Chandran Cain
+ * 
+ * @version 11.8.23
+ * 
+ * Application view class
+ */
 public class ApplicationView extends JFrame {
+	/**
+	 * String containing the app's name.
+	 */
     private final String APPLICATION_NAME = "the App";
+    /**
+     * String containing the about buttons name.
+     */
     private final String ABOUT_BUTTON_NAME = "About";
+    /**
+     * Int containing width of app.
+     */
     private final int appWidth = 500;
+    /**
+     * Int containing height of app.
+     */
     private final int appHeight = 500;
+    /**
+     * Creates AboutController object.
+     */
     private final AboutController aboutController;
+    /**
+     * Creates ProfileController object.
+     */
     private final ProfileController profileController;
+    /**
+     * ApplicationView constructor.
+     */
     public ApplicationView() {
         aboutController = new AboutController();
         profileController = new ProfileController();
@@ -23,25 +54,58 @@ public class ApplicationView extends JFrame {
         this.setLayout(new BorderLayout());
         this.add(displayAboutBtn(), BorderLayout.NORTH);
     }
-
+    /**
+     * Creates about button.
+     * @return
+     */
     private JButton displayAboutBtn() {
         JButton aboutBtn = new JButton(ABOUT_BUTTON_NAME);
         aboutBtn.addActionListener(new AboutListener(this, aboutController, profileController));
         return aboutBtn;
     }
-
+    /**
+     * returns app's width.
+     * @return
+     */
     public int getAppWidth() {
         return appWidth;
     }
-
+    /**
+     * returns app's height.
+     * @return
+     */
     public int getAppHeight() {
         return appHeight;
     }
-
+    /*
+     * @author Darrell Green, Jr. (DJ Green)
+     * @author Zarif Mazumder
+     * @author Harman Singh
+     * @author Vindhriko Chandran Cain
+     * 
+     * @version 11.8.23
+     * 
+     * AboutListener class
+     */
     private static class AboutListener implements ActionListener {
+    	/**
+    	 * Creates ApplicationView object.
+    	 */
         private final ApplicationView applicationView;
+        /**
+         * creates AboutController object.
+         */
         private final AboutController aboutController;
+        /**
+         * Creates ProfileController object.
+         */
         private final ProfileController profileController;
+        /**
+         * AboutListener constructor.
+         * @param applicationView
+         * @param aboutController
+         * @param profileController
+         */
         public AboutListener(
                 ApplicationView applicationView,
                 AboutController aboutController,
@@ -50,6 +114,10 @@ public class ApplicationView extends JFrame {
             this.aboutController = aboutController;
             this.profileController = profileController;
         }
+        /**
+         * opens the about screen.
+         * @param e
+         */
         public void actionPerformed(ActionEvent e) {
             try {
                 AboutScreen dialog = new AboutScreen(applicationView, aboutController, profileController);
