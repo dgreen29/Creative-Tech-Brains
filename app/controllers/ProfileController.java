410fc55770d3b455b89a51b1e04add8c7fe21b80
Vin <72899108+Kaz-png@users.noreply.github.com>
2023-11-09 05:24:56 -0800

    Update ProfileController.java

--- app/controllers/ProfileController.java
+++ app/controllers/ProfileController.java
@@ -2,13 +2,31 @@ package app.controllers;
 
 import app.models.Profile;
 
+/*
+ * @author Darrell Green, Jr. (DJ Green)
+ * @author Zarif Mazumder
+ * @author Harman Singh
+ * @author Vindhriko Chandran Cain
+ * 
+ * @version 11.8.23
+ * 
+ * Profile controller class
+ */
 public final class ProfileController {
+	// profile controller constructor.
     public ProfileController() {}
 
+    /**
+     * gets name.
+     * @return Profile.getName().
+     */
     public String getName() {
         return Profile.getName();
     }
-
+    /**
+     * gets email.
+     * @return Profile.getEmail().
+     */
     public String getEmail() {
         return Profile.getEmail();
     }
