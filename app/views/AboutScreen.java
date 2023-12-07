a90cb1783a19051720e5a3f357ce2a34d0a4cf07
Zarif Mazumder <137818290+zarifzm@users.noreply.github.com>
2023-12-06 19:32:02 -0800

    Merge correction (#46)
    * displayText <-> displayProfile
    
    * Display team names
    
    * Format layouts
    
    * Fix layout, display Version and Profile
    
    * formatting
    
    * test commit
    
    * test commit
    
    * Fix return for get AboutController.getVersion
    
    * Reset java.awt import
    
    * Remove old test.java
    
    * typos
    
    * Add .idea to .gitignore
    
    * Add team member names
    
    * spacing
    
    * User interaction diagram for adding item to project budget
    
    * Design deliverable WIP
    
    * Deliverable 2 diagrams
    
    * Deliverable 2 diagrams
    
    * Added a ProfileScreen class in view package that is used by the user to import and export their profile.
    
    * Design deliverable WIP
    
    * Deliverable 2 diagrams
    
    * Deliverable 2 diagrams
    
    * WIP
    
    * Iteration 2 Back-end
    
    * Iteration 2 - Javadocs WIP
    
    * Iteration 2
    
    * Iteration 2 - comments and tests
    
    * Code Review Guide
    
    * Properly catch stream close operations
    
    * Properly catch stream close operations with Java ARM
    
    * base layout for code deliverable
    
    * base back-end layout for code deliverable
    
    * code review suggestions
    
    * code review suggestions
    
    * WIP
    
    * UI Changes
    
    * Merge correction
    
    ---------
    
    Co-authored-by: Harman <hsingh4845@gmail.com>

---
+++ app/views/AboutScreen.java
@@ -0,0 +1,63 @@
+package app.views;
+
+import app.Main;
+import app.controllers.AboutController;
+import app.controllers.ProfileController;
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Displays the team and version info.
+ * @author Zarif Mazumder
+ */
+public final class AboutScreen extends JDialog {
+    private static final String TITLE_NAME = "About";
+    private final AboutController aboutController;
+    private final ProfileController profileController;
+
+    public AboutScreen(ProfileController profileController) {
+        aboutController = new AboutController();
+        this.profileController = profileController;
+        this.setTitle(TITLE_NAME);
+        this.setModalityType(ModalityType.APPLICATION_MODAL);
+        this.setSize(Main.APP_WIDTH + 200, Main.APP_HEIGHT / 2);
+        this.setLocationRelativeTo(null);
+        this.setLayout(new BorderLayout());
+        JPanel northPanel = new JPanel(new BorderLayout());
+        northPanel.add(displayProfile(), BorderLayout.WEST);
+        northPanel.add(displayVersion(), BorderLayout.EAST);
+        this.add(northPanel, BorderLayout.NORTH);
+        this.add(displayTeam(), BorderLayout.CENTER);
+    }
+
+    /**
+     * @author Zarif Mazumder
+     * @return profile <code>JLabel</code>
+     */
+    private JLabel displayProfile() {
+        JLabel heading = new JLabel();
+        heading.setText("This app is registered to: " + profileController.getName());
+        return heading;
+    }
+
+    /**
+     * @author Zarif Mazumder
+     * @return team <code>JLabel</code>
+     */
+    private JLabel displayTeam() {
+        JLabel text = new JLabel();
+        String[] team = aboutController.getTeam();
+        text.setText("This app provided by: " + String.join(", ", team));
+        return text;
+    }
+
+    /**
+     * @author Zarif Mazumder
+     * @return version <code>JLabel</code>
+     */
+    private JLabel displayVersion() {
+        JLabel text = new JLabel();
+        text.setText("Version: " + aboutController.getVersion());
+        return text;
+    }
+}
