19b54f83ff494bec6f8d03865da9156684c557b8
Zarif Mazumder <137818290+zarifzm@users.noreply.github.com>
2023-12-11 17:06:50 -0800

    Project budget (#53)
    * displayText <-> displayProfile
    
    * Display team names
    
    * Format layouts
    
    * Fix layout, display Version and Profile
    
    * formatting
    
    * test commit
    
    * test commit
    
    * Fix return for get AboutController.getVersion
    
    * Reset java.awt import
    
    * Remove old test.java
    
    * typos
    
    * Add .idea to .gitignore
    
    * Add team member names
    
    * spacing
    
    * User interaction diagram for adding item to project budget
    
    * Design deliverable WIP
    
    * Deliverable 2 diagrams
    
    * Deliverable 2 diagrams
    
    * Added a ProfileScreen class in view package that is used by the user to import and export their profile.
    
    * Design deliverable WIP
    
    * Deliverable 2 diagrams
    
    * Deliverable 2 diagrams
    
    * WIP
    
    * Iteration 2 Back-end
    
    * Iteration 2 - Javadocs WIP
    
    * Iteration 2
    
    * Iteration 2 - comments and tests
    
    * Code Review Guide
    
    * Properly catch stream close operations
    
    * Properly catch stream close operations with Java ARM
    
    * base layout for code deliverable
    
    * base back-end layout for code deliverable
    
    * code review suggestions
    
    * code review suggestions
    
    * WIP
    
    * UI Changes
    
    * Merge correction
    
    * Navigation bar
    
    * Implementation of Detail
    
    * Project select bar WIP
    
    * Validate name and email
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP database
    
    * WIP database
    
    * WIP database
    
    * Project checklist
    
    * Project budget
    
    ---------
    
    Co-authored-by: Harman <hsingh4845@gmail.com>

--- app/views/BudgetView.java
+++ app/views/BudgetView.java
@@ -3,12 +3,12 @@ package app.views;
 import app.Main;
 import app.controllers.BudgetController;
 import app.controllers.ProfileController;
-import app.controllers.ProjectController;
+import app.models.Entry;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import javax.swing.filechooser.FileNameExtensionFilter;
+import java.math.BigDecimal;
+import java.util.LinkedList;
 
 /**
  * Displays the screen showing the <code>Budget</code> of a project.
@@ -18,67 +18,96 @@ public class BudgetView extends JFrame {
     private static final String TITLE_NAME = "Budget";
     private final BudgetController budgetController;
     private final ProfileController profileController;
-    private final ProjectController projectController;
-
-    private final JTextField nameField;
-    private final JTextField costField;
-    private final JTextField quantityField;
-    private final JButton uploadButton;
-    private final DefaultListModel<String> listModel;
-    private final JList<String> itemList;
 
     public BudgetView(ProfileController profileController) {
         this.profileController = profileController;
-        projectController = profileController.getProjectController();
-        budgetController = new BudgetController(projectController);
+        budgetController = new BudgetController(profileController.getProjectController());
         this.setTitle(TITLE_NAME);
         this.setSize(Main.APP_WIDTH, Main.APP_HEIGHT);
         this.setLocationRelativeTo(null);
         this.setLayout(new BorderLayout());
         this.setJMenuBar(new NavigationBar(profileController));
-
-        JPanel newItemPanel = new JPanel(new GridLayout(0, 2));
-
-        newItemPanel.add(new JLabel("Name: "));
-        nameField = new JTextField();
-        newItemPanel.add(nameField);
-
-        newItemPanel.add(new JLabel("Cost: "));
-        costField = new JTextField();
-        newItemPanel.add(costField);
-
-        newItemPanel.add(new JLabel("Quantity: "));
-        quantityField = new JTextField();
-        newItemPanel.add(quantityField);
-
-        newItemPanel.add(new JLabel("Upload image: "));
-        uploadButton = new JButton("Upload");
-        uploadButton.addActionListener(e -> {
-            JFileChooser chooser = new JFileChooser();
-            FileNameExtensionFilter filter = new FileNameExtensionFilter(
-                    "JPG & GIF Images", "jpg", "gif");
-            chooser.setFileFilter(filter);
-            int returnVal = chooser.showOpenDialog(null);
-            if(returnVal == JFileChooser.APPROVE_OPTION) {
-                File selectedFile = chooser.getSelectedFile();
-                // handle the selected image file
-            }
-        });
-        newItemPanel.add(uploadButton);
-
-        listModel = new DefaultListModel<>();
-        itemList = new JList<>(listModel);
-        JScrollPane scrollPane = new JScrollPane(itemList);
-
-        this.add(newItemPanel, BorderLayout.NORTH);
-        this.add(scrollPane, BorderLayout.CENTER);
-
-        JButton addItemButton = new JButton("Add Item");
-        addItemButton.addActionListener(e -> {
-            String newItem = "Name: " + nameField.getText() + ", Cost: " + costField.getText() + ", QTY: " + quantityField.getText();
-            listModel.addElement(newItem);
-        });
-        this.add(addItemButton, BorderLayout.SOUTH);
+        this.add(displayContent(), BorderLayout.CENTER);
         this.add(new ProjectSelectBar(profileController), BorderLayout.PAGE_END);
+    }
+
+    /**
+     * @author Zarif Mazumder
+     * @return content
+     */
+    private JScrollPane displayContent() {
+        JPanel content = new JPanel();
+        content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
+        LinkedList<Entry> entries = budgetController.getEntries();
+        JTable table = displayEntries(entries);
+        table.setPreferredScrollableViewportSize(new Dimension(
+                table.getPreferredSize().width, table.getRowHeight() * table.getRowCount()));
+        JScrollPane tablePane = new JScrollPane(table);
+        JPanel addEntryBtn = displayAddEntryButton();
+        content.add(tablePane);
+        content.add(addEntryBtn);
+        return new JScrollPane(content);
+    }
+
+    /**
+     * @author Zarif Mazumder
+     * @param entries data
+     * @return <code>JTable</code> of entries
+     */
+    private JTable displayEntries(LinkedList<Entry> entries) {
+        if (entries.isEmpty()) return new JTable();
+        String[] columnNames = {"cost", "item", "quantity"};
+        String[][] rowData = new String[entries.size()][3];
+        for (int i = 0; i < entries.size(); i++) {
+            Entry entry = entries.get(i);
+            rowData[i][0] = entry.getCost().toString();
+            rowData[i][1] = entry.getName();
+            rowData[i][2] = String.valueOf(entry.getQuantity());
+        }
+        return new JTable(rowData, columnNames);
+    }
+
+    /**
+     * @author Zarif Mazumder
+     * @return add item button
+     */
+    private JPanel displayAddEntryButton() {
+        JPanel checkBoxItem = new JPanel();
+        JButton addItemBtn = new JButton("+");
+        JLabel costLabel = new JLabel("Cost");
+        JTextField cost = new JTextField(4);
+        JLabel itemLabel = new JLabel("Item");
+        JTextField item = new JTextField(4);
+        JLabel quantityLabel = new JLabel("Quantity");
+        JTextField quantity = new JTextField(4);
+        addItemBtn.addActionListener(e -> {
+            String costText = cost.getText().trim();
+            String nameText = item.getText().trim();
+            String quantityText = quantity.getText().trim();
+            if (isValidEntry(costText, nameText, quantityText)) {
+                budgetController.addEntry(BigDecimal.valueOf(Double.parseDouble(costText)), nameText,
+                        Integer.parseInt(quantityText));
+            }
+            Main.setCurrentView(new BudgetView(profileController));
+        });
+        checkBoxItem.add(addItemBtn);
+        checkBoxItem.add(costLabel);
+        checkBoxItem.add(cost);
+        checkBoxItem.add(itemLabel);
+        checkBoxItem.add(item);
+        checkBoxItem.add(quantityLabel);
+        checkBoxItem.add(quantity);
+        return checkBoxItem;
+    }
+
+    private boolean isValidEntry(String costText, String nameText, String quantityText) {
+        if (costText.isEmpty() || nameText.isEmpty() || quantityText.isEmpty()) return false;
+        try {
+            Double.parseDouble(costText);
+            Integer.parseInt(quantityText);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
--- app/views/DetailView.java
+++ app/views/DetailView.java
@@ -38,7 +38,7 @@ public class DetailView extends JFrame {
         content.add(doc, BorderLayout.CENTER);
         JButton saveBtn = new JButton("Save");
         saveBtn.addActionListener(e -> {
-            detailController.setText(doc.getText());
+            detailController.setText(doc.getText().trim());
             saveBtn.setText("Saved!");
             saveBtn.setForeground(Color.GREEN);
             Timer timer = new Timer(500, f -> {
